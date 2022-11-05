c51b97618d18f937c186076fbd818d1c09118e54
Jassdalla <jasdal22@student.wintec.ac.nz>
2022-11-06 07:56:23 +1300

    Changes- intro, dialog and coin
    3 changes made

--- Intro.gd
+++ Intro.gd
@@ -9,7 +9,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play("Fade in")
-	yield(get_tree().create_timer(6),"timeout")
+	yield(get_tree().create_timer(3),"timeout")
 	$AnimationPlayer.play("Fade out")
 	yield(get_tree().create_timer(3),"timeout")
 	get_tree().change_scene("res://Scenes/World/TitleScene.tscn")
